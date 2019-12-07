400a7dbd4ec231aeb5925636d6456ba12216a839
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2019-12-07 10:31:55 +0530

    added total catches and total time played

--- fruitEater.js
+++ fruitEater.js
@@ -6,6 +6,7 @@ stdin.setRawMode(true);
 let fruits = require("./fruits.json");
 const fruitsQue = [];
 const catcherInfo = { catcher: "🗑 🗑 🗑 🗑", left: 20, right: 28, rows: 10 };
+const gameInfo = { totalCatches: 0, time: new Date().valueOf() };
 
 const displayCatcher = function(x, y) {
   stdout.cursorTo(x, y);
@@ -41,6 +42,12 @@ const displayFruits = function() {
 const exitGame = function() {
   process.stderr.write("\x1B[?25h");
   console.clear();
+  stdout.write(`Total Catches : ${gameInfo.totalCatches}\n`);
+  stdout.write(
+    `Total Time Taken : ${Math.floor(
+      (new Date().valueOf() - gameInfo.time) / 1000
+    )}\n`
+  );
   process.exit(0);
 };
 
@@ -64,11 +71,29 @@ const isSingleFruitDied = function(fruit) {
   const { left, right } = catcherInfo;
   if (fruit.y == catcherInfo.rows && fruit.x >= left && fruit.x <= right) {
     fruitsQue.splice(fruitsQue.indexOf(fruit), 1);
+    gameInfo.totalCatches += 1;
   }
 };
 
 const isEat = function() {
-  fruitsQue.forEach(fruit);
+  fruitsQue.forEach(isSingleFruitDied);
+};
+
+const changePosition = function(userKeyStroke) {
+  if (userKeyStroke === 0 && catcherInfo.left > 1) {
+    catcherInfo.left -= 3;
+    catcherInfo.right -= 3;
+  }
+  if (userKeyStroke === 1 && catcherInfo.right < columns) {
+    catcherInfo.left += 3;
+    catcherInfo.right += 3;
+  }
+  if (userKeyStroke === 2 && catcherInfo.rows > 2) {
+    catcherInfo.rows -= 1;
+  }
+  if (userKeyStroke === 3 && catcherInfo.rows < rows) {
+    catcherInfo.rows += 1;
+  }
 };
 
 const moveEater = function(userDir) {
@@ -78,31 +103,19 @@ const moveEater = function(userDir) {
   }
   const userKeyStroke = keyStrokes.indexOf(userDir);
   if (userDir != -1) {
-    if (userKeyStroke === 0 && catcherInfo.left > 1) {
-      catcherInfo.left -= 3;
-      catcherInfo.right -= 3;
-    }
-    if (userKeyStroke === 1 && catcherInfo.right < columns) {
-      catcherInfo.left += 3;
-      catcherInfo.right += 3;
-    }
-    if (userKeyStroke === 2 && catcherInfo.rows > 2) {
-      catcherInfo.rows -= 3;
-    }
-    if (userKeyStroke === 3 && catcherInfo.rows < rows) {
-      catcherInfo.rows += 3;
-    }
+    changePosition(userKeyStroke);
     stdout.cursorTo(catcherInfo.rows, catcherInfo.left);
   }
   isEat();
 };
 
 const main = function() {
+  console.clear();
   setInterval(() => {
     isEat();
   }, 1);
   setInterval(fallChars, 500);
-  setInterval(storeNextFruitToPrint, 5000);
+  setInterval(storeNextFruitToPrint, 3000);
   stdin.on("data", moveEater);
 };
 
